eeb90095ab96cba42ae365989d9b5f4208ced875
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-24 09:37:00 +0900

    ボタン押しでテキスト変更させるところまでをコミット

--- app/src/main/java/jp/lgs/hello_first_sample/MainActivity.kt
+++ app/src/main/java/jp/lgs/hello_first_sample/MainActivity.kt
@@ -2,11 +2,22 @@ package jp.lgs.hello_first_sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showTxt.setText(R.string.helloFirst)
+
+        button.setOnClickListener {
+            showTxt.setText(R.string.helloAgain)
+        }
+
     }
+
+
+
 }
