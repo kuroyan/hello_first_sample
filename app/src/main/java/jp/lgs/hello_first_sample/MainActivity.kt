3806b902f027a85d45306e76907253275c6c8612
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-24 12:10:28 +0900

    メインからサブへサブからメインへの切り替えがうまくいった版

--- app/src/main/java/jp/lgs/hello_first_sample/MainActivity.kt
+++ app/src/main/java/jp/lgs/hello_first_sample/MainActivity.kt
@@ -1,7 +1,9 @@
 package jp.lgs.hello_first_sample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +14,40 @@ class MainActivity : AppCompatActivity() {
 
         showTxt.setText(R.string.helloFirst)
 
-        button.setOnClickListener {
-            showTxt.setText(R.string.helloAgain)
-        }
+        //button.setOnClickListener {
+        //    showTxt.setText(R.string.helloAgain)
+        //}
 
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun onButtonClick(view: View) {
+        val intent = Intent(applicationContext,SubActivity::class.java)
+        startActivity(intent)
+    }
 
 
 }
--- app/src/main/java/jp/lgs/hello_first_sample/SubActivity.kt
+++ app/src/main/java/jp/lgs/hello_first_sample/SubActivity.kt
@@ -1,7 +1,9 @@
 package jp.lgs.hello_first_sample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SubActivity : AppCompatActivity() {
 
@@ -9,4 +11,34 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun onButtonClick(view: View) {
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
